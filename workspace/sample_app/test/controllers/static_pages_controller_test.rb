a58f0f3cd415b8db94556acc0116bde03a1f7cb9
deepitapai <deepita.pai2011@vit.ac.in>
2015-11-07 15:55:35 +0530

    Without Sign Up

--- workspace/sample_app/test/controllers/static_pages_controller_test.rb
+++ workspace/sample_app/test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,27 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","Home|Ruby on Rails tutorial Sample App"
+    assert_select "title","Ruby on Rails tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title","Help|Ruby on Rails tutorial Sample App"
+    assert_select "title","Help | Ruby on Rails tutorial Sample App"
 
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title","About|Ruby on Rails tutorial Sample App"
+    assert_select "title","About | Ruby on Rails tutorial Sample App"
 
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title","Contact | Ruby on Rails tutorial Sample App"
+
+  end
 end
